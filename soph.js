adf42c393b0dedb94be2e534cc6a7e9103adf1ce
mhkmhsh <mmahesh12@gmail.com>
2025-02-13 11:47:47 -0500

    Commiting for now, rly need to figure out the modal issue

--- soph.js
+++ soph.js
@@ -1,105 +1,133 @@
-// ---------Responsive-navbar-active-animation-----------
-function test() {
-  var tabsNewAnim = $('#navbarSupportedContent');
-  var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-  var activeItemNewAnim = tabsNewAnim.find('.active');
-  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-  var itemPosNewAnimTop = activeItemNewAnim.position();
-  var itemPosNewAnimLeft = activeItemNewAnim.position();
-  $(".hori-selector").css({
-    "top": itemPosNewAnimTop.top + "px",
-    "left": itemPosNewAnimLeft.left + "px",
-    "height": activeWidthNewAnimHeight + "px",
-    "width": activeWidthNewAnimWidth + "px"
-  });
+// --------- Responsive Navbar Active Animation ---------
+function updateNavbarSelector() {
+  const activeItem = $('#navbarSupportedContent').find('.active');
+  const itemHeight = activeItem.innerHeight();
+  const itemWidth = activeItem.innerWidth();
+  const itemPosition = activeItem.position();
 
-  $("#navbarSupportedContent").on("click", "li", function (e) {
-    $('#navbarSupportedContent ul li').removeClass("active");
-    $(this).addClass('active');
-    var activeWidthNewAnimHeight = $(this).innerHeight();
-    var activeWidthNewAnimWidth = $(this).innerWidth();
-    var itemPosNewAnimTop = $(this).position();
-    var itemPosNewAnimLeft = $(this).position();
-    $(".hori-selector").css({
-      "top": itemPosNewAnimTop.top + "px",
-      "left": itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
-    });
+  $(".hori-selector").css({
+    "top": `${itemPosition.top}px`,
+    "left": `${itemPosition.left}px`,
+    "height": `${itemHeight}px`,
+    "width": `${itemWidth}px`
   });
 }
 
 $(document).ready(function () {
-  setTimeout(function () { test(); });
+  setTimeout(updateNavbarSelector);
 });
 
+// Update active item position on window resize
 $(window).on('resize', function () {
-  setTimeout(function () { test(); }, 500);
+  setTimeout(updateNavbarSelector, 500);
 });
 
+// Toggle navbar collapse on mobile
 $(".navbar-toggler").click(function () {
   $(".navbar-collapse").slideToggle(300);
-  setTimeout(function () { test(); });
+  setTimeout(updateNavbarSelector);
 });
 
-// --------------add active class-on another-page move----------
-jQuery(document).ready(function ($) {
-  var path = window.location.pathname.split("/").pop();
-
-  if (path == '') {
-    path = 'index.html';
-  }
-
-  var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
+// --------- Add Active Class on Page Load ---------
+$(document).ready(function () {
+  const path = window.location.pathname.split("/").pop() || 'index.html';
+  const target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
   target.parent().addClass('active');
 });
 
+// --------- Scroll Animation for Menu ---------
 $(window).scroll(function () {
-  var scrollTop = $(window).scrollTop();
-  $(".menu").css("transform", "translate(-50%, calc(-50% + " + scrollTop / 3 + "px))");
+  const scrollTop = $(window).scrollTop();
+  $(".menu").css("transform", `translate(-50%, calc(-50% + ${scrollTop / 3}px))`);
 });
 
-
- // CAR SWIPER STUFF FROM THEIR JS FILE
- const swiper = new Swiper('.slider-wrapper', {
-  
+// --------- Swiper Carousel ---------
+const swiper = new Swiper('.slider-wrapper', {
   loop: true,
   grabCursor: true,
   spaceBetween: 30,
-
-  // If we need pagination
+  autoplay: {
+    delay: 3000,  // 3 seconds
+    disableOnInteraction: false, // Keep autoplay running even after user interaction
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
     dynamicBullets: true
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  //responsive breakpoints
-  breakpoints:{
-    0: {
-    slidesPerView:1
-  },
-
-  620: {
-    slidesPerView:2
-  },
-  1024: {
-    slidesPerView:3
-  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    620: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 }
   }
-
 });
 
+// --------- Modal Gallery ---------
+let activeSlide = 1;
+const carousel = document.querySelector('.carousel');
+const items = document.querySelectorAll('.carousel .item');
+const itemCount = items.length;
+
+function updateModalSlider() {
+  // Reset previous active items and transitions
+  const itemOldActive = document.querySelector('.carousel .item.active');
+  if (itemOldActive) itemOldActive.classList.remove('active');
+  
+  const itemOldOther_1 = document.querySelector('.carousel .item.other_1');
+  if (itemOldOther_1) itemOldOther_1.classList.remove('other_1');
+  
+  const itemOldOther_2 = document.querySelector('.carousel .item.other_2');
+  if (itemOldOther_2) itemOldOther_2.classList.remove('other_2');
+  
+  // Set new active items
+  const other_1 = activeSlide - 1 < 0 ? itemCount - 1 : activeSlide - 1;
+  const other_2 = activeSlide + 1 >= itemCount ? 0 : activeSlide + 1;
+
+  items[activeSlide].classList.add('active');
+  items[other_1].classList.add('other_1');
+  items[other_2].classList.add('other_2');
+
+  // Reset image animations
+  items.forEach(item => {
+    const img = item.querySelector('.image img');
+    const caption = item.querySelector('.image figcaption');
+    img.style.animation = 'none';
+    caption.style.animation = 'none';
+    void item.offsetWidth;  // Trigger reflow
+    img.style.animation = '';
+    caption.style.animation = '';
+  });
+
+  // Reset autoplay after each manual change
+  clearInterval(autoPlay);
+  autoPlay = setInterval(nextSlide, 5000);
+}
+
+// Next slide
+function nextSlide() {
+  carousel.classList.remove('prev');
+  carousel.classList.add('next');
+  activeSlide = (activeSlide + 1) % itemCount;
+  updateModalSlider();
+}
+
+// Previous slide
+function prevSlide() {
+  carousel.classList.remove('next');
+  carousel.classList.add('prev');
+  activeSlide = activeSlide - 1 < 0 ? itemCount - 1 : activeSlide - 1;
+  updateModalSlider();
+}
+
+// Set up event listeners for next/prev buttons
+document.getElementById('next').onclick = nextSlide;
+document.getElementById('prev').onclick = prevSlide;
+
+// Autoplay every 5 seconds
+let autoPlay = setInterval(nextSlide, 5000);
 
 
-
-//sites gallery 2 js beginning
-
-// sites gallery 2 js ending
